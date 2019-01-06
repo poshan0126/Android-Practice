ecc1418945330e55fa75f2e96fd3bf6bb92fb878
poshan0126 <poshan0126@gmail.com>
2019-01-06 19:20:54 +0545

    not completed fragment will continue again

--- FragmentsinAndroid/app/src/main/java/com/example/poshanpandey/fragmentsinandroid/MainActivity.java
+++ FragmentsinAndroid/app/src/main/java/com/example/poshanpandey/fragmentsinandroid/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.poshanpandey.fragmentsinandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.FragmentManager;
+import android.app.Fragment;
+import android.app.Fragment.*;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void ChangeFragment(View view) {
+        Fragment fragment;
+        if (view == findViewById(R.id.button)) {
+            fragment = new fragment1();
+            FragmentManager fm = getFragmentManager();
+
+
+        }
+        if (view == findViewById(R.id.button2)) {
+            fragment = new fragment2();
+
+        }
     }
 }
