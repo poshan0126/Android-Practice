a6d91708f4d98328994adf592173f9be001edb6f
poshan0126 <poshan0126@gmail.com>
2019-01-18 13:53:46 +0545

    AutoCompleteView Done and Android Time pIcker processing

---
+++ AndroidTimePicker/app/src/main/java/com/example/androidtimepicker/AndroidTimePicker2.java
@@ -0,0 +1,13 @@
+package com.example.androidtimepicker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AndroidTimePicker2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_android_time_picker2);
+    }
+}
--- AndroidTimePicker/app/src/main/java/com/example/androidtimepicker/MainActivity.java
+++ AndroidTimePicker/app/src/main/java/com/example/androidtimepicker/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.androidtimepicker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TimePicker time_picker;
+    private Button button_show_time;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showTime();
+    }
+    public void showTime(){
+        time_picker = (TimePicker)findViewById(R.id.timePicker1);
+        button_show_time = (Button)findViewById(R.id.button);
+        time_picker.setIs24HourView(true);
+        button_show_time.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(getBaseContext(),time_picker.getCurrentHour() + ":" + time_picker.getCurrentMinute(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
     }
 }
