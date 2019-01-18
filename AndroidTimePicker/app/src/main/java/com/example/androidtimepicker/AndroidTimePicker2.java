b21ff20b3a62747a8574debecbb8da3c209e8cd3
poshan0126 <poshan0126@gmail.com>
2019-01-18 14:21:29 +0545

    Android Time pIcker completed

--- AndroidTimePicker/app/src/main/java/com/example/androidtimepicker/AndroidTimePicker2.java
+++ AndroidTimePicker/app/src/main/java/com/example/androidtimepicker/AndroidTimePicker2.java
@@ -1,13 +1,50 @@
 package com.example.androidtimepicker;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class AndroidTimePicker2 extends AppCompatActivity {
+    private Button time_picker3;
+    static final int Dialog_Id = 0;
+    int hour_x ;
+    int minute_x;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_time_picker2);
+        showTimePickerDialog();
+    }
+    @Override
+    protected Dialog onCreateDialog(int id){
+     if(id==Dialog_Id)
+            return new TimePickerDialog(AndroidTimePicker2.this,kTimePickerListener,hour_x,minute_x,false);
+     return null;
+    }
+    protected  TimePickerDialog.OnTimeSetListener kTimePickerListener = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            hour_x = hourOfDay;
+            minute_x = minute;
+            Toast.makeText(AndroidTimePicker2.this,hour_x + ":"+ minute_x,Toast.LENGTH_SHORT).show();
+
+        }
+    };
+    public void showTimePickerDialog(){
+        time_picker3 = (Button)findViewById(R.id.button4);
+        time_picker3.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        showDialog(Dialog_Id);
+                    }
+                }
+        );
     }
 }
--- AndroidTimePicker/app/src/main/java/com/example/androidtimepicker/MainActivity.java
+++ AndroidTimePicker/app/src/main/java/com/example/androidtimepicker/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.androidtimepicker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private TimePicker time_picker;
     private Button button_show_time;
+    private Button change_button;
 
 
     @Override
@@ -17,6 +19,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         showTime();
+        change_button = (Button)findViewById(R.id.button2);
+        change_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openbetterMethod();
+            }
+        });
+    }
+    public void openbetterMethod(){
+        Intent intent = new Intent(this,AndroidTimePicker2.class);
+        startActivity(intent);
     }
     public void showTime(){
         time_picker = (TimePicker)findViewById(R.id.timePicker1);
