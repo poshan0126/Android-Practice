259e9fdb21caec24c279c964a97084bcfc4ee1dd
poshan0126 <poshan0126@gmail.com>
2019-01-27 22:11:29 +0545

    app for Nischal

--- Stopwatch/app/src/main/java/com/example/stopwatch/MainActivity.java
+++ Stopwatch/app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         chronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
             @Override
             public void onChronometerTick(Chronometer chronometer) {
-                if ((SystemClock.elapsedRealtime() - chronometer.getBase()) >= 10000) {
+                if ((SystemClock.elapsedRealtime() - chronometer.getBase()) >= 1000000) {
                     chronometer.setBase(SystemClock.elapsedRealtime());
                     Toast.makeText(MainActivity.this, "Bing!", Toast.LENGTH_SHORT).show();
                 }
