03d10dc39f59bf2abb7051e1fd8e46373d7775d4
poshan0126 <poshan0126@gmail.com>
2019-02-10 19:30:21 +0545

    using Background app ie. services in android learnt.

---
+++ ServicesInAndroid/app/src/main/java/com/example/servicesinandroid/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.servicesinandroid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void startService(View view){
+        Intent intent = new Intent(this,TheService.class);
+        startService(intent);
+    }
+    public void stopService(View view){
+        Intent intent = new Intent(this,TheService.class);
+        stopService(intent);
+    }
+}
